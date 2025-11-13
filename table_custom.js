f26948587d83459bf10cb75e5e23aab1973b6808
amandamoooooore <amandalittler@me.com>
2025-11-13 13:06:44 +0000

    Update table_custom.js

--- table_custom.js
+++ table_custom.js
@@ -157,7 +157,7 @@ looker.plugins.visualizations.add({
                 background:#111a44;
                 color:#111a44;
                 padding:4px 10px;
-                border-bottom:1px solid #e0e0e0;
+                border-bottom:none;
                 white-space:nowrap;">
             &nbsp;
           </th>`;
